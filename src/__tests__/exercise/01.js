06654039e6d418ba4db33405b4536c12800454ce
coofzilla <jeric614@yahoo.com>
2022-05-29 14:23:37 +0900

    click event w/dispatchEvent

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -17,35 +17,36 @@ beforeEach(() => {
 })
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to
   const div = document.createElement('div')
-  // 🐨 append the div to document.body
+
   document.body.append(div)
 
-  // 🐨 use createRoot to render the <Counter /> to the div
   const root = createRoot(div)
   act(() => root.render(<Counter />))
 
-  // 🐨 get a reference to the increment and decrement buttons:
-
   const [decrement, increment] = div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
+
   const message = div.firstChild.querySelector('div')
 
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
   expect(message.textContent).toBe('Current count: 0')
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  act(() => increment.click())
-  // 🐨 assert the message.textContent
-  expect(message.textContent).toBe('Current count: 1')
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
 
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  act(() => decrement.click())
-  // 🐨 assert the message.textContent
+  act(() => increment.dispatchEvent(incrementClickEvent))
+
+  expect(message.textContent).toBe('Current count: 1')
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
+  act(() => decrement.dispatchEvent(decrementClickEvent))
+
   expect(message.textContent).toBe('Current count: 0')
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  div.remove()
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
 
 /* eslint no-unused-vars:0 */
