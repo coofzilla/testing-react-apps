f6e750b77b1326064d7dab87e5003a2e231149fa
coofzilla <jeric614@yahoo.com>
2022-06-03 13:06:08 +0900

    refactor w/screen.getByText

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,16 +2,17 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+
+import {render, screen, fireEvent} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  render(<Counter />)
+
+  const increment = screen.getByText(/increment/i)
+  const decrement = screen.getByText(/decrement/i)
+
+  const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
   fireEvent.click(increment)
