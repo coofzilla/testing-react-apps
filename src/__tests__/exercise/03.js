385eeae8015ead6ab47050929153617e5cc00eb6
coofzilla <jeric614@yahoo.com>
2022-06-03 15:50:09 +0900

    refactor w/userevent and comment explanation f/async await req

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,20 +3,25 @@
 
 import * as React from 'react'
 
-import {render, screen, fireEvent} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+//@testing-library/user-event version 14 has made all APIs async
+
+test('counter increments and decrements when the buttons are clicked', async () => {
   render(<Counter />)
 
-  const increment = screen.getByText(/increment/i)
-  const decrement = screen.getByText(/decrement/i)
+  //   const increment = screen.getByText(/increment/i)
+  //   const decrement = screen.getByText(/decrement/i)
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const decrement = screen.getByRole('button', {name: /decrement/i})
 
   const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
